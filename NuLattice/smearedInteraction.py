d9ae8eb41a36a434cfd581387b0ab9b1873c4d1f
mrothma1 <mrothma1@vols.utk.edu>
2025-09-25 13:40:40 -0400

    fninshed(hopefully) implementing smeared interaction

--- NuLattice/smearedInteraction.py
+++ NuLattice/smearedInteraction.py
@@ -6,7 +6,7 @@ import NuLattice.constants as consts
 
 def smearMat(myL, sL, sNL):
     """
-    gets the smearing matrix to get the 
+    gets the smearing matrix to get the local and nonlocal creatopm amd annihilation operators
 
     :param myL: number of lattice sites in each direction
     :type myL:  int
@@ -62,9 +62,70 @@ def smearedInteract(myL, c0, sL, sNL):
     nonLocSq = contract('ij, jk -> ik', nonLocal, nonLocal)
     return c0 / 2 * contract('ij, jk, kl -> il', nonLocSq, fSL, nonLocSq)
 
-def onePionEx(myL, bpi):
+def onePionEx(myL, bpi, spin, a_lat):
+    """
+    computes the potential for one pion exchange
 
-    return 0
+    :param myL: number of lattice sites in each direction
+    :type myL:  int
+    :param bpi: parameter to remove short-distance lattice artifacts
+    :type bpi: float
+    :param spin: Not sure why this is specified tbh
+    :type spin: int
+    :param a_lat: lattice spacing in fm
+    :type a_lat: float
+    """
+    coupling = -(consts.g_A / (2 * a_lat * consts.f_pi)) ** 2
+    cg = np.zeros([2, 2, 2, 4])
+    val = 1 / np.sqrt(2)
+    cg[0, 1, 0, 0] = val
+    cg[0, 1, 0, 0] = -val
+    cg[0,0,1,2] = 1.0
+    cg[0,1,1,1] = val
+    cg[1,0,1,1] = val
+    cg[1,1,1,0] = 1.0 
+
+    pauli = np.zeros([2, 2, 3], dtype=complex)
+    pauli[0, 1, 0] = 1.0
+    pauli[1, 0, 0] = 1.0
+    pauli[0, 1, 1] = -1j
+    pauli[1, 0, 1] = 1j
+    pauli[0, 0, 2] = 1.0
+    pauli[1, 1, 2] = -1.0
+
+    pp = (np.linspace(0,myL**3-1, num=myL**3)).astype(int)
+    px = pp % myL
+    py = ((pp - px) / myL) % myL
+    pz = (pp-px-py*myL) / myL ** 2
+    px = ((px+myL/2.0) % myL) - myL/2.0
+    py = ((py+myL/2.0) % myL) - myL/2.0
+    pz = ((pz+myL/2.0) % myL) - myL/2.0
+    
+    val = 2 * np.pi / myL
+    q2 = ((np.square(px) + np.square(py) + np.square(pz)) * (val) ** 2).astype(float)
+    qqx = val * px
+    qqy = val * py
+    qqz = val * pz
+
+    potOPE = [[0]*(2 * spin + 1)] * (2 * spin + 1)
+    for sz in range(-spin, spin+1):
+        for szp in range(-spin, spin+1):
+            potMom = np.zeros([1, myL**3])
+            for nSpin in range(16):
+                is1 = int(nSpin % 2)
+                is2 = int(((nSpin - is1) % 4) / 2)
+                is1p = int(((nSpin - is1 - 2 * (is2)) % 8) / 4)
+                is2p = int((nSpin - is1 - 2 * (is2) - 4 * (is1p)) / 8)
+                potMom +=   (coupling * cg[is1, is2, spin, sz + spin] * cg[is1p, is2p, spin, szp + spin]
+                            *np.real(pauli[is1, is1p, 0] * qqx + pauli[is1, is1p, 1] * qqy + pauli[is1, is1p, 2] * qqz)
+                            *np.real(pauli[is2, is2p, 0] * qqx + pauli[is2, is2p, 1] * qqy + pauli[is2, is2p, 2] * qqz)
+                            /(q2 + consts.m_pi **2) * np.exp(-bpi * (q2 + consts.m_pi ** 2))
+                            )
+            np.reshape(potMom, shape=[myL, myL, myL])
+            ftPotMom = np.fft.ifftn(potMom)
+            np.reshape(ftPotMom, shape = [myL**3, 1])
+            potOPE[sz + spin][szp + spin] = ftPotMom 
+    return np.diag(potOPE[0][0])
 
 def interact(interMat, lattice, myL, spin=2, isospin=2):
     """
@@ -129,7 +190,7 @@ def interact(interMat, lattice, myL, spin=2, isospin=2):
                                             matele.append([indx1, indx2, indx3, indx4, interMat[pos1, pos2]])
     return matele
 
-def Tkin(lattice, myL, spin=2, isospin=2):
+def Tkin(lattice, myL, a_lat, spin=2, isospin=2):
     """
     computes 1-body kinetic energy matrix elements. Really: the negative dimensionless laplacian 
 
@@ -146,12 +207,13 @@ def Tkin(lattice, myL, spin=2, isospin=2):
     :rtype:     list[(int, int, float)]
     """
     mat = []
+    unit = lat.phys_unit(a_lat)
     for site in lattice:
         i = site[0]
         j = site[1]
         k = site[2]
         #diagonal element from each direction
-        val = 15.0 / 2.0
+        val = 15.0 / 2.0 * unit
         for tz in range(isospin):
             for sz in range(spin):
                 state1 = [i, j, k, tz, sz]
@@ -164,8 +226,8 @@ def Tkin(lattice, myL, spin=2, isospin=2):
         ry2 = lat.right(ry, myL=myL)
         rz = lat.right(k, myL=myL)
         rz2 = lat.right(rz, myL=myL)
-        val1 = -4.0 / 3.0
-        val2 = 1.0 / 12.0
+        val1 = -4.0 / 3.0 * unit
+        val2 = 1.0 / 12.0 * unit
         for tz in range(isospin):
             for sz in range(spin):
                 #hop one in x
@@ -216,3 +278,30 @@ def Tkin(lattice, myL, spin=2, isospin=2):
                 mat.append([indx1, indx2, val2])
                 mat.append([indx2, indx1, val2]) #adds a hop-to-the left matrix element
     return mat
+
+def get_full_int(myL, bpi, c0, sL, sNL, a_lat, spin = 2, isospin = 2):
+    """
+    Takes the parameters needed for the smeared interaction, and returns the one and two body matrix elements
+
+    :param myL:     number of lattice sites in each direction
+    :type myL:      int
+    :param bpi: parameter to remove short-distance lattice artifacts
+    :type bpi: float
+    :param c0:  strength of short-range interactions
+    :type c0:   float
+    :param sL:  strength of the local interaction
+    :type sL:   float
+    :param sNL:  strength of the nonlocal interaction
+    :type sNL:   float
+    :param a_lat: lattice spacing in fm
+    :type a_lat: float
+    :param spin:    Optional; number of spin degrees of freedom
+    :type spin:     int
+    :param isospin: Optional; number of isospin degrees of freedom
+    :type isospin:  int    
+    """
+    lattice = lat.get_lattice(myL)
+    kin = Tkin(lattice, myL, a_lat, spin, isospin)
+    interMat = onePionEx(myL, bpi, 0, a_lat) + smearedInteract(myL, c0, sL, sNL)
+    full_int = interact(interMat, lattice, myL, spin, isospin)
+    return kin, np.real(full_int)
