52eef65b5a40087ea38b2477e9ee9d83737c8676
mrothma1 <mrothma1@vols.utk.edu>
2025-09-25 09:21:09 -0400

    finished smeared interaction, still need one pion exchange

--- NuLattice/constants.py
+++ NuLattice/constants.py
@@ -6,3 +6,6 @@ hbarc = 197.3269804  # MeV fm
 mass_p= 938.27208943 # MeV
 mass_n= 939.56542194 # MeV
 mass  = 0.5*(mass_p+mass_n)
+g_A = 1.29
+m_pi = 134.98 # MeV
+f_pi = 92.2 #MeV
---
+++ NuLattice/smearedInteraction.py
@@ -0,0 +1,218 @@
+import numpy as np
+from opt_einsum import contract
+import copy
+import NuLattice.lattice as lat
+import NuLattice.constants as consts
+
+def smearMat(myL, sL, sNL):
+    """
+    gets the smearing matrix to get the 
+
+    :param myL: number of lattice sites in each direction
+    :type myL:  int
+    :param sL:  strength of the local interaction
+    :type sL:   float
+    :param sNL:  strength of the nonlocal interaction
+    :type sNL:   float
+    """
+
+    nn = (np.linspace(0,myL**3-1, num=myL**3)).astype(int)
+    nx = nn % myL
+    ny = ((nn - nx) / myL) % myL
+    nz = (nn-nx-ny*myL) / myL ** 2
+    nxp = (((nx+1) % myL) + ny*myL + nz*myL**2).astype(int)
+    nxm = (((nx-1) % myL) + ny*myL + nz*myL**2).astype(int)
+    nyp = (nx + ((ny+1) % myL)*myL + nz*myL**2).astype(int)
+    nym = (nx + ((ny-1) % myL)*myL + nz*myL**2).astype(int)
+    nzp = (nx + ny*myL + ((nz+1) % myL)*myL**2).astype(int)
+    nzm = (nx + ny*myL + ((nz-1) % myL)*myL**2).astype(int)
+    smear = np.zeros([myL**3, myL**3])
+    for i in range(myL**3):
+        smear[nn[i]][nxp[i]] += 1
+        smear[nn[i]][nxm[i]] += 1
+        smear[nn[i]][nyp[i]] += 1
+        smear[nn[i]][nym[i]] += 1
+        smear[nn[i]][nzp[i]] += 1
+        smear[nn[i]][nzm[i]] += 1
+        
+    id = np.identity(myL**3)
+    locMat = sL*smear + id
+    nonLocMat = sNL*smear + id
+    return locMat, nonLocMat
+
+def smearedInteract(myL, c0, sL, sNL):
+    """
+    gets the smeared interaction as a function of two positions
+
+    :param myL: number of lattice sites in each direction
+    :type myL:  int
+    :param c0:  strength of short-range interactions
+    :type c0:   float
+    :param sL:  strength of the local interaction
+    :type sL:   float
+    :param sNL:  strength of the nonlocal interaction
+    :type sNL:   float
+    """
+
+    local, nonLocal = smearMat(myL, sL, sNL)
+    locOrigin = np.zeros([myL**3])
+    locOrigin[0] = 1.0
+
+    fSL = np.diag(contract('ij, jk, k -> i', local, local, locOrigin))
+    nonLocSq = contract('ij, jk -> ik', nonLocal, nonLocal)
+    return c0 / 2 * contract('ij, jk, kl -> il', nonLocSq, fSL, nonLocSq)
+
+def onePionEx(myL, bpi):
+
+    return 0
+
+def interact(interMat, lattice, myL, spin=2, isospin=2):
+    """
+    computes matrix elements for 2-body onsite contacts
+    
+    :param interMat: matrix that takes two lattice positions and returns the strength of the interation
+    :type interMat: 2D numpy array with dimension myL ** 3 by myL ** 3
+    :param lattice: list of lattice sites returned by get_lattice
+    :type lattice:  list[(int, int, int)]
+    :param myL:     number of lattice sites in each direction
+    :type myL:      int
+    :param spin:    Optional; number of spin degrees of freedom
+    :type spin:     int
+    :param isospin: Optional; number of isospin degrees of freedom
+    :type isospin:  int    
+    :return:    list of lists [i, j, k, l, value] where i, j and k, l are indices of two particles
+                in the single-particle basis, and value is the value of the matrix element <ij||kl>. 
+                All elements have i<j and k<l
+    :rtype:         list[(int, int, int, int, float)]
+    """
+    matele = []
+    for site1 in lattice:
+        for tz1 in range(isospin):
+            for sz1 in range(spin):
+                stat1 = copy.deepcopy(site1)
+                stat1.append(tz1)
+                stat1.append(sz1)
+                indx1 = lat.state2index(stat1, myL=myL, spin=spin, isospin=isospin)
+                for site2 in lattice:
+                    for tz2 in range(isospin):
+                        for sz2 in range(spin):
+                            if tz1 == tz2 and sz1 == sz2: #not asymetric under exchange
+                                continue
+                            stat2 = copy.deepcopy(site2)
+                            stat2.append(tz2)
+                            stat2.append(sz2)
+                            indx2 = lat.state2index(stat2, myL=myL, spin=spin, isospin=isospin)
+                            if indx2 <= indx1: #we only keep properly ordered two-body states
+                                continue
+                            for tz3 in range(isospin):
+                                for sz3 in range(spin):
+                                    stat3 = copy.deepcopy(site2)
+                                    stat3.append(tz3)
+                                    stat3.append(sz3)
+                                    indx3 = lat.state2index(stat3, myL=myL, spin=spin, isospin=isospin)
+                                    for tz4 in range(isospin):
+                                        if tz1 + tz2 != tz3 + tz4: #Tz is not conserved
+                                            continue
+                                        for sz4 in range(spin):
+                                            if sz1+sz2 != sz3+sz4: #Sz is not conserved
+                                                continue
+                                            if tz3 == tz4 and sz3 == sz4: #not asymetric under exchange
+                                                continue
+                                            stat4 = copy.deepcopy(site1)
+                                            stat4.append(tz4)
+                                            stat4.append(sz4)
+                                            indx4 = lat.state2index(stat4, myL=myL, spin=spin, isospin=isospin)
+                                            if indx4 <= indx3:
+                                                continue
+                                            pos1 = site1[0] + site1[1]* myL + site1[2] * myL ** 2
+                                            pos2 = site2[0] + site2[1]* myL + site2[2] * myL ** 2
+                                            matele.append([indx1, indx2, indx3, indx4, interMat[pos1, pos2]])
+    return matele
+
+def Tkin(lattice, myL, spin=2, isospin=2):
+    """
+    computes 1-body kinetic energy matrix elements. Really: the negative dimensionless laplacian 
+
+    :param lattice: list of lattice sites returned by get_lattice
+    :type lattice:  list[(int, int, int)]
+    :param myL:     number of lattice sites in each direction
+    :type myL:      int
+    :param spin:    Optional; number of spin degrees of freedom
+    :type spin:     int
+    :param isospin: Optional; number of isospin degrees of freedom
+    :type isospin:  int    
+    :return:    list of tuples [i, j, value] where i and j are indices in the single-particle 
+                basis, and value is the value of the matrix element Tij
+    :rtype:     list[(int, int, float)]
+    """
+    mat = []
+    for site in lattice:
+        i = site[0]
+        j = site[1]
+        k = site[2]
+        #diagonal element from each direction
+        val = 15.0 / 2.0
+        for tz in range(isospin):
+            for sz in range(spin):
+                state1 = [i, j, k, tz, sz]
+                indx1 = lat.state2index(state1, myL=myL, spin=spin, isospin=isospin)
+                mat.append([indx1, indx1, val])
+
+        rx = lat.right(i, myL=myL)
+        rx2 = lat.right(rx, myL=myL)
+        ry = lat.right(j, myL=myL)
+        ry2 = lat.right(ry, myL=myL)
+        rz = lat.right(k, myL=myL)
+        rz2 = lat.right(rz, myL=myL)
+        val1 = -4.0 / 3.0
+        val2 = 1.0 / 12.0
+        for tz in range(isospin):
+            for sz in range(spin):
+                #hop one in x
+                state1 = [i, j, k, tz, sz]
+                state2 = [rx, j, k, tz, sz]
+                indx1 = lat.state2index(state1, myL=myL, spin=spin, isospin=isospin)
+                indx2 = lat.state2index(state2, myL=myL, spin=spin, isospin=isospin)
+                mat.append([indx1, indx2, val1])
+                mat.append([indx2, indx1, val1]) #adds a hop-to-the left matrix element
+
+                #hop two in x
+                state1 = [i, j, k, tz, sz]
+                state2 = [rx2, j, k, tz, sz]
+                indx1 = lat.state2index(state1, myL=myL, spin=spin, isospin=isospin)
+                indx2 = lat.state2index(state2, myL=myL, spin=spin, isospin=isospin)
+                mat.append([indx1, indx2, val2])
+                mat.append([indx2, indx1, val2]) #adds a hop-to-the left matrix element
+
+                #hop one in y
+                state1 = [i, j, k, tz, sz]
+                state2 = [i, ry, k, tz, sz]
+                indx1 = lat.state2index(state1, myL=myL, spin=spin, isospin=isospin)
+                indx2 = lat.state2index(state2, myL=myL, spin=spin, isospin=isospin)
+                mat.append([indx1, indx2, val1])
+                mat.append([indx2, indx1, val1]) #adds a hop-to-the left matrix element
+
+                #hop two in y
+                state1 = [i, j, k, tz, sz]
+                state2 = [i, ry2, k, tz, sz]
+                indx1 = lat.state2index(state1, myL=myL, spin=spin, isospin=isospin)
+                indx2 = lat.state2index(state2, myL=myL, spin=spin, isospin=isospin)
+                mat.append([indx1, indx2, val2])
+                mat.append([indx2, indx1, val2]) #adds a hop-to-the left matrix element
+
+                #hop one in z
+                state1 = [i, j, k, tz, sz]
+                state2 = [i, j, rz, tz, sz]
+                indx1 = lat.state2index(state1, myL=myL, spin=spin, isospin=isospin)
+                indx2 = lat.state2index(state2, myL=myL, spin=spin, isospin=isospin)
+                mat.append([indx1, indx2, val1])
+                mat.append([indx2, indx1, val1]) #adds a hop-to-the left matrix element
+
+                #hop two in z
+                state1 = [i, j, k, tz, sz]
+                state2 = [i, j, rz2, tz, sz]
+                indx1 = lat.state2index(state1, myL=myL, spin=spin, isospin=isospin)
+                indx2 = lat.state2index(state2, myL=myL, spin=spin, isospin=isospin)
+                mat.append([indx1, indx2, val2])
+                mat.append([indx2, indx1, val2]) #adds a hop-to-the left matrix element
+    return mat
