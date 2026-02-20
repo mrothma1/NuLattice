47823431e844582f23e2dd3f67b138f6a111bb38
mrothma1 <mrothma1@vols.utk.edu>
2026-02-20 12:17:27 -0500

    OPE should be done now

--- NuLattice/smearedInteraction.py
+++ NuLattice/smearedInteraction.py
@@ -291,7 +291,6 @@ def f_SS(myL, bpi, a_lat):
     :rtype:         complex numpy array of dimension 3,3, myL, myL, myL
     """
     m_pi = consts.m_pi_0 * a_lat
-    # m_pi = (consts.m_pi_0 + 2.0 * consts.m_pi_c)* a_lat / 3.0
     q =np.fft.fftfreq(myL, 1/ (2.0 * np.pi))
     qx, qy, qz = np.meshgrid(q, q, q, indexing="ij")
 
@@ -424,80 +423,6 @@ def onePionEx(myL, bpi, a_lat, lattice, verbose = False, mult = 1, spin=2, isosp
         print('Done')
 
     return ret
-
-def onePionEx_old(myL, bpi, spin, a_lat):
-    """
-    computes the potential for one pion exchange
-
-    :param myL: number of lattice sites in each direction
-    :type myL:  int
-    :param bpi: parameter to remove short-distance lattice artifacts
-    :type bpi: float
-    :param spin: Not sure why this is specified tbh, only spin 0 is used in the program
-    :type spin: int
-    :param a_lat: lattice spacing divided by hbar c
-    :type a_lat: float
-    """
-    coupling = (-(consts.g_A / (2 * a_lat * consts.f_pi)) ** 2)
-    mass = consts.m_pi * a_lat
-
-    cg = np.zeros([2, 2, 2, 4])
-    val = 1 / np.sqrt(2)
-    cg[0, 1, 0, 0] = val
-    cg[1, 0, 0, 0] = -val
-    cg[0,0,1,2] = 1.0
-    cg[0,1,1,1] = val
-    cg[1,0,1,1] = val
-    cg[1,1,1,0] = 1.0 
-
-    pauli = np.zeros([2, 2, 3], dtype=complex)
-    pauli[0, 1, 0] = 1.0
-    pauli[1, 0, 0] = 1.0
-    pauli[0, 1, 1] = -1j
-    pauli[1, 0, 1] = 1j
-    pauli[0, 0, 2] = 1.0
-    pauli[1, 1, 2] = -1.0
-
-    pp = (np.linspace(0,myL**3-1, num=myL**3)).astype(int)
-    px = pp % myL
-    py = ((pp - px) / myL) % myL
-    pz = (pp-px-py*myL) / myL ** 2
-    px = ((px+myL/2.0) % myL) - myL/2.0
-    py = ((py+myL/2.0) % myL) - myL/2.0
-    pz = ((pz+myL/2.0) % myL) - myL/2.0
-    
-    val = 2 * np.pi / myL
-    q2 = ((np.square(px) + np.square(py) + np.square(pz)) * (val) ** 2).astype(float)
-    qqx = val * px
-    qqy = val * py
-    qqz = val * pz
-
-    potOPE = [[0 for _ in range(2 * spin + 1)] for _ in range(2 * spin + 1)]
-    for sz in range(-spin, spin+1):
-        for szp in range(-spin, spin+1):
-            potMom = (np.zeros([1, myL**3])).astype(complex)
-            for nSpin in range(16):
-                is1 = int(nSpin % 2)
-                is2 = int(((nSpin - is1) % 4) / 2)
-                is1p = int(((nSpin - is1 - 2 * (is2)) % 8) / 4)
-                is2p = int((nSpin - is1 - 2 * (is2) - 4 * (is1p)) / 8)
-                potMom +=   (coupling 
-                            *cg[is1, is2, spin, sz + spin] 
-                            *cg[is1p, is2p, spin, szp + spin]
-                            *(pauli[is1, is1p, 0] * qqx 
-                                     + pauli[is1, is1p, 1] * qqy 
-                                     + pauli[is1, is1p, 2] * qqz)
-                            *(pauli[is2, is2p, 0] * qqx 
-                                     + pauli[is2, is2p, 1] * qqy 
-                                     + pauli[is2, is2p, 2] * qqz)
-                            /(q2 + mass ** 2) * np.exp(-bpi * (q2 + mass ** 2))
-                            )
-            potMom_reshaped = np.reshape(potMom, (myL, myL, myL))
-            ftPotMom = np.fft.ifftn(potMom_reshaped)
-            ftPotMom_reshaped = np.reshape(ftPotMom, (myL**3))
-            potOPE[sz + spin][szp + spin] = ftPotMom_reshaped
-    
-    return (np.diag((potOPE[0][0])))
 
 def indConv(ind, myL):
     """
