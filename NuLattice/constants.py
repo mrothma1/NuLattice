ab606c29dda52363e6fff5a533f9e0af830195fa
mrothma1 <mrothma1@vols.utk.edu>
2026-05-15 14:11:26 -0400

    Updated constants and added constants_NLEFT

--- NuLattice/constants.py
+++ NuLattice/constants.py
@@ -5,7 +5,7 @@ Provides useful constants to be used in the rest of the code
 hbarc = 197.3269804  # MeV fm
 mass_p= 938.27208943 # MeV
 mass_n= 939.56542194 # MeV
-mass  = 938.92#0.5*(mass_p+mass_n)
+mass  = 0.5*(mass_p+mass_n)
 g_A = 1.287
 m_pi_0 = 134.98 # MeV
 m_pi_c = 139.57 #MeV
---
+++ NuLattice/constants_NLEFT.py
@@ -0,0 +1,10 @@
+"""
+Provides useful constants to be used in the rest of the code as defined in the NLEFT papers
+"""
+
+hbarc = 197  # MeV fm
+mass  = 938.92
+g_A = 1.287
+m_pi_0 = 134.98 # MeV
+m_pi_c = 139.57 #MeV
+f_pi = 92.2 #MeV
